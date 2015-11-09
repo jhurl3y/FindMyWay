7d10c27b52acd984b94cb20cc47456046680cb17
James Hurley <james.hurley@exordo.com>
2015-11-09 16:17:59 +0000

    Added google api key

--- app/src/main/java/com/fyp/findmyway/MapsActivity.java
+++ app/src/main/java/com/fyp/findmyway/MapsActivity.java
@@ -1,21 +1,39 @@
 package com.fyp.findmyway;
 
+import android.content.Context;
+
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.location.LocationListener;
+import android.location.Location;
+import android.location.LocationManager;
 import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
+import android.util.Log;
 
+import com.google.android.gms.location.LocationServices;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
 
-public class MapsActivity extends FragmentActivity {
+public class MapsActivity extends FragmentActivity implements LocationListener {
 
+    private LocationManager lm;
     private GoogleMap mMap; // Might be null if Google Play services APK is not available.
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_maps);
+
+        GoogleApiClient client = new GoogleApiClient.Builder(this)
+                .addConnectionCallbacks(this)
+                .addOnConnectionFailedListener(this)
+                .addApi(LocationServices.API)
+                .build();
+        client.connect();
+        lm = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
+//        lm.requestLocationUpdates(LocationManager.NETWORK_PROVIDER, 1000, 0, this);
         setUpMapIfNeeded();
     }
 
@@ -60,6 +78,26 @@ public class MapsActivity extends FragmentActivity {
      * This should only be called once and when we are sure that {@link #mMap} is not null.
      */
     private void setUpMap() {
+        // lm.getLastKnownLocation();
         mMap.addMarker(new MarkerOptions().position(new LatLng(0, 0)).title("Marker"));
     }
+
+    @Override
+    public void onLocationChanged(Location location) {
+    }
+
+//    @Override
+//    public void onStatusChanged(String provider, int status, Bundle extras) {
+//        Log.e("GPS", "status changed to " + provider + " [" + status + "]");
+//    }
+//
+//    @Override
+//    public void onProviderEnabled(String provider) {
+//        Log.e("GPS", "provider enabled " + provider);
+//    }
+//
+//    @Override
+//    public void onProviderDisabled(String provider) {
+//        Log.e("GPS", "provider disabled " + provider);
+//    }
 }
